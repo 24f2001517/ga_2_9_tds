457bb80ecec1d1bd29f3787d9295388309e0b654
24f2001517 <24f2001517@ds.study.iitm.ac.in>
2026-07-08 23:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ rate_limits = {}
 
 
 # Enable CORS
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
-
 
 # -------------------------
 # Rate limiting middleware
